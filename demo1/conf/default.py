2a333096137a5b0ca3a45ed9190afeeca52341be
13295039231@163.com <13295039231@163.com>
2019-04-11 10:09:52 +0800

    ddd

--- demo1/conf/default.py
+++ demo1/conf/default.py
@@ -30,8 +30,8 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'hzy-demo'
-APP_TOKEN = '541075c8-d862-452d-b35d-63d656044ac2'
+APP_ID = 'jiaqian'
+APP_TOKEN = '16a17905-73d9-41f7-a307-0fd0ce9196ab'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'https://paas.blueking.com'
 BK_PAAS_INNER_HOST = os.environ.get('BK_PAAS_INNER_HOST', BK_PAAS_HOST)
