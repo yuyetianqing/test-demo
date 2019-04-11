abf81897cd33792ac2873a6347065550dd0387bd
13295039231@163.com <13295039231@163.com>
2019-04-11 09:36:15 +0800

    ccc

--- framework/conf/default.py
+++ framework/conf/default.py
@@ -25,7 +25,7 @@ from django.conf.global_settings import *  # noqa
 APP_ID = ''
 APP_TOKEN = ''
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = ''
+BK_PAAS_HOST = 'http://paas.zorkdata.com:80'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
